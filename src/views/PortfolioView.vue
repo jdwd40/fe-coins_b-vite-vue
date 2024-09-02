f5b0cb4313f13c1676cc6d99a63a9299595e0a30
jdwd40 <jd@jdwd40.com>
2024-09-02 10:04:07 +0100

    updated protfolio layout

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen bg-gray-100 p-4">
     <div class="container mx-auto">
-      <h1 class="text-3xl font-bold text-gray-800 mb-6">My Portfolio</h1>
+      <h1 class="text-3xl font-bold text-gray-800 mb-6">Portfolio</h1>
       <div v-if="loading" class="text-gray-700">Loading...</div>
       <div v-else>
         <!-- User Info -->
