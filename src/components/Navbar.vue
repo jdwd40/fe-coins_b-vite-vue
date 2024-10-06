1554ebbcd47d74060d8aeac134c7889301f1ca87
jdwd40 <jd@jdwd40.com>
2024-10-06 18:13:31 +0100

    added darker color scheme to nav-bar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,26 +1,26 @@
 <template>
-  <nav class="bg-blue-500 p-4">
+  <nav class="bg-gray-800 p-4">
     <div class="container mx-auto">
       <div class="flex flex-wrap justify-between items-center">
-        <div class="text-white text-lg font-bold mb-2 md:mb-0">
+        <div class="text-gray-100 text-lg font-bold mb-2 md:mb-0">
           Fantasy Crypto Exchange
         </div>
-        <button @click="toggleMenu" class="md:hidden text-white">
+        <button @click="toggleMenu" class="md:hidden text-gray-100">
           <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
           </svg>
         </button>
         <div :class="{'hidden': !isMenuOpen, 'block': isMenuOpen}" class="w-full md:flex md:items-center md:w-auto">
           <div class="text-sm md:flex-grow">
-            <router-link class="block mt-4 md:inline-block md:mt-0 text-white hover:text-gray-200 mr-4" to="/">Home</router-link>
-            <router-link class="block mt-4 md:inline-block md:mt-0 text-white hover:text-gray-200 mr-4" to="/market">Market</router-link>
-            <router-link v-if="!user" class="block mt-4 md:inline-block md:mt-0 text-white hover:text-gray-200 mr-4" to="/login">Login</router-link>
-            <router-link v-if="!user" class="block mt-4 md:inline-block md:mt-0 text-white hover:text-gray-200 mr-4" to="/register">Register</router-link>
-            <router-link v-if="user" class="block mt-4 md:inline-block md:mt-0 text-white hover:text-gray-200 mr-4" to="/portfolio">Portfolio</router-link>
+            <router-link class="block mt-4 md:inline-block md:mt-0 text-gray-100 hover:text-gray-400 mr-4" to="/">Home</router-link>
+            <router-link class="block mt-4 md:inline-block md:mt-0 text-gray-100 hover:text-gray-400 mr-4" to="/market">Market</router-link>
+            <router-link v-if="!user" class="block mt-4 md:inline-block md:mt-0 text-gray-100 hover:text-gray-400 mr-4" to="/login">Login</router-link>
+            <router-link v-if="!user" class="block mt-4 md:inline-block md:mt-0 text-gray-100 hover:text-gray-400 mr-4" to="/register">Register</router-link>
+            <router-link v-if="user" class="block mt-4 md:inline-block md:mt-0 text-gray-100 hover:text-gray-400 mr-4" to="/portfolio">Portfolio</router-link>
           </div>
           <div v-if="user" class="mt-4 md:mt-0">
-            <span class="block md:inline-block text-white mr-4">{{ user.username }} ({{ user.funds ? Number(user.funds).toFixed(2) : '0.00' }})</span>
-            <a href="#" class="block md:inline-block text-white hover:text-gray-200" @click.prevent="logout">Logout</a>
+            <span class="block md:inline-block text-gray-100 mr-4">{{ user.username }} ({{ user.funds ? Number(user.funds).toFixed(2) : '0.00' }})</span>
+            <a href="#" class="block md:inline-block text-gray-100 hover:text-gray-400" @click.prevent="logout">Logout</a>
           </div>
         </div>
       </div>
@@ -52,3 +52,7 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+/* Add any additional custom styles here */
+</style>
